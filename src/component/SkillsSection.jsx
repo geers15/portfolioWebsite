be7fb90e7d820d8db3d4f0945f686a593a45e422
geers15 <gersong178@gmail.com>
2025-10-09 23:42:39 -0700

    Skills Section completed

--- src/component/SkillsSection.jsx
+++ src/component/SkillsSection.jsx
@@ -1,3 +1,5 @@
+import {useState} from "react";
+import {cn} from "@/lib/utils.js";
 
 const skills=[
     {name:"HTML/CSS", level:95,category:"frontend"},
@@ -15,15 +17,28 @@ const skills=[
     {name:"VS Code", level:10,category:"tools"},
 ]
 
+const categories = ["all","frontend","backend", "tools"];
 
 export const SkillsSection = () => {
+    const [activeCategory, setActiveCategory] = useState("all")
+
+    const filteredSkills = skills.filter((skills) => activeCategory === "all" || skills.category === activeCategory)
     return <section id="skills" className="py-24 px-4 relative bg-secondary/30">
         <div className="container mx-auto max-w-5xl">
             <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
                 My <span className="text-primary"> Skills </span>
             </h2>
+
+            <div className="flex flex-wrap justify-center gap-4 mb-12">
+                {categories.map((category, key) => (
+                    <button key={key} className={cn("px-5 py-2 rounded-full transition-colors duration-300 capitalize",
+                    activeCategory === category ? "bg-primary text-primary-foreground" : "bg-secondary/70 text-foreground hover:bd-secondary")} onClick={()=> setActiveCategory(category)}>
+                        {category}
+                    </button>
+                ))}
+            </div>
             <div className="grid grid-col-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-                {skills.map((skill,key) => (
+                {filteredSkills.map((skill,key) => (
                     <div key={key} className="bg-card p-6 rounded-lg shadow-xs card-hover" >
                         <div className="text-left mb-4">
                             <h3 className="font-semibold text-lg">
