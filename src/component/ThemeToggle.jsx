8dc977504bf02a36c58b0ebfa6e47e6e776ccbab
geers15 <gersong178@gmail.com>
2025-10-16 22:52:59 -0700

    theme toggle edit

--- src/component/ThemeToggle.jsx
+++ src/component/ThemeToggle.jsx
@@ -31,7 +31,29 @@ export const ThemeToggle = () => {
              setIsDarkMode(true);
          }
      }
-    return <button onClick={toggleTheme} className={cn("fixed top-5 right-5 z-50 p-2 rounded-full transition duration-300",
-        "focus:outline-hidden"
-        )}> {isDarkMode ? <Sun className="h-6 w-6 text-yellow-300"/> : <Moon className="h-6 w-6 text-blue-900"/>}</button>
+    return <button onClick={toggleTheme}
+                   className={cn("fixed top-5 right-5 z-50 p-2 rounded-full transition duration-300",
+        "focus:outline-hidden",
+
+                       "2xl: top-4 2xl:right-6",
+                       // Large laptops / big screens (≥1440px)
+                       "xl:top-15 xl:right-10",
+
+                       // Standard laptop (≥1024px)
+                       "lg:top-15 lg:right-9",
+
+                       // Tablets / small laptops (≥768px)
+                       "md:top-15 md:right-8",
+
+                       //Small tablets (≥640px)
+                       "sm:top-14 sm:right-6",
+
+                       //Small phones (≤640px)
+                       "top-16 right-8 bottom-auto"
+        )}>
+        {isDarkMode ?
+            <Sun className="h-6 w-6 text-yellow-300"/>
+            :
+            <Moon className="h-6 w-6 text-blue-900"/>}
+     </button>
 }
