0caf91d2e9070a00774ed607365a2a75b0b4db07
geers15 <gersong178@gmail.com>
2025-10-16 23:57:39 -0700

    message handler

--- src/component/ContactSection.jsx
+++ src/component/ContactSection.jsx
@@ -14,7 +14,7 @@ export const ContactSection = () => {
 
         emailjs
             .sendForm(
-                "service_gmail_smtp",    // ✅ Your Service ID
+                "service_bt8ufjx",    // ✅ Your Service ID
                 "template_vwn7rd5",
                 e.target,
                 "MeLXX-YeCs9YhQ0pE"    // ✅ Your Public Key
